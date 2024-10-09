1f9847bb138364f60df76fd42de3e54cdf3c26f9
joaomdp <joaomanoeldpereira@gmail.com>
2024-10-08 22:46:27 -0300

    continue project portifolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Hero from "./components/Hero"
 import About from "./components/About"
 import Technologies from "./components/Technologies"
 import Experience from "./components/Experience"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
@@ -17,6 +19,8 @@ const App = () => {
         <About />
         <Technologies />
         <Experience />
+        <Projects />
+        <Contact />
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
-import logo from "../assets/kevinRushLogo.png"
+import logo from "../assets/jd.png"
 import { FaLinkedin } from "react-icons/fa"
 import { FaGithub } from "react-icons/fa"
-import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa"
 
 
@@ -9,13 +8,18 @@ const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img className="mx-2 w-10" src={logo} alt="logo" />
+                <img className="mx-2 w-24" src={logo} alt="logo" />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-                <FaLinkedin />
-                <FaGithub />
-                <FaSquareXTwitter />
-                <FaInstagram />
+                <a href="https://www.linkedin.com/in/joaomanoeldev/">
+                    <FaLinkedin />
+                </a>
+                <a href="https://github.com/joaomdp?tab=repositories">
+                    <FaGithub />
+                </a>
+                <a href="https://www.instagram.com/joaomdp/">
+                    <FaInstagram />
+                </a>
             </div>
         </nav>
     )
